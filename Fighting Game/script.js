7e3d785120f188ae3a5a32fbea56ab938d52a1a9
mohit-1606 <mohit1606gandhi@gmail.com>
2023-08-28 01:26:39 +0530

    -no-edit

--- Fighting Game/script.js
+++ Fighting Game/script.js
@@ -1,26 +1,24 @@
-let playButton = document.getElementById('play')
-let resultDiv = document.getElementById('result')
-let p1NameDiv = document.getElementById('p1Name')
-let p2NameDiv = document.getElementById('p2Name')
-let p1HealthDiv = document.getElementById('p1Health')
-let p2HealthDiv = document.getElementById('p2Health')
+let playButton = document.getElementById("play");
+let resultDiv = document.getElementById("result");
+let p1NameDiv = document.getElementById("p1Name");
+let p2NameDiv = document.getElementById("p2Name");
+let p1HealthDiv = document.getElementById("p1Health");
+let p2HealthDiv = document.getElementById("p2Health");
 
-const updateGame = (p1,p2,gameState) => {
-  // Update the DOM with the names and the latest health of players
-  p1NameDiv.innerText = p1.name
-  p2NameDiv.innerText = p2.name
-  p1HealthDiv.innerText = p1.health
-  p2HealthDiv.innerText = p2.health
-  
-  // if either player health is less than or equal to zero then 1.) set .isOver to true and update DOM to declare winner
-  if (p1.health <= 0 || p2.health <= 0) { 
+const updateGame = (p1, p2, gameState) => {
+  p1NameDiv.innerText = p1.name;
+  p2NameDiv.innerText = p2.name;
+  p1HealthDiv.innerText = p1.health;
+  p2HealthDiv.innerText = p2.health;
+
+  if (p1.health <= 0 || p2.health <= 0) {
     game.isOver = true;
-    gameState = game.isOver
-    result.innerText = game.declareWinner(game.isOver,p1,p2)
-    return gameState
-  } 
-}
-// Player Class
+    gameState = game.isOver;
+    result.innerText = game.declareWinner(game.isOver, p1, p2);
+    return gameState;
+  }
+};
+
 class Player {
   constructor(name, health, attackDamage) {
     this.name = name;
@@ -28,135 +26,109 @@ class Player {
     this.attackDmg = attackDamage;
   }
 
-  strike (player, enemy, attackDmg) { // strike method 
-    
-    let damageAmount = Math.ceil(Math.random() * attackDmg) // damage amount to be random number
-    
-    enemy.health -= damageAmount
-    
-    updateGame(p1,p2,gameState) // call the updateGame function to update the DOM
-    
-    return `${player.name} attacks ${enemy.name} for ${damageAmount}` 
+  strike(player, enemy, attackDmg) {
+    let damageAmount = Math.ceil(Math.random() * attackDmg);
+
+    enemy.health -= damageAmount;
+
+    updateGame(p1, p2, gameState);
+
+    return `${player.name} attacks ${enemy.name} for ${damageAmount}`;
   }
-  
-  heal (player) { // heal method
-    
-    let hpAmount = Math.ceil(Math.random() * 5) // heal amount to be random number between 1 and 5
-    
-    player.health += hpAmount
-    
-    updateGame(p1,p2,gameState) // call the updateGame function to update the DOM
-    
-    return `${player.name} heals for ${hpAmount} HP!`
+
+  heal(player) {
+    let hpAmount = Math.ceil(Math.random() * 5);
+
+    player.health += hpAmount;
+
+    updateGame(p1, p2, gameState);
+
+    return `${player.name} heals for ${hpAmount} HP!`;
   }
 }
 
-// Create the Game class with all it's attributes and methods to run a match 
 class Game {
   constructor() {
     this.isOver = false;
   }
 
-  declareWinner(isOver,p1, p2) {
-    let message
-    
+  declareWinner(isOver, p1, p2) {
+    let message;
+
     if (isOver == true && p1.health <= 0) {
       message = `${p2.name} WINS!`;
+    } else if (isOver == true && p2.health <= 0) {
+      message = `${p1.name} WINS!`;
     }
-    
-    else if(isOver == true && p2.health <= 0) {
-      message = `${p1.name} WINS!`
-    } 
-    console.log(isOver, p1.health, p2.health)
- 
-    document.getElementById('victory').play() // victory sound is played
-   
-    return message
+    console.log(isOver, p1.health, p2.health);
+
+    document.getElementById("victory").play();
+
+    return message;
   }
 
-  // Reset the players health back to it's original state and isOver to FALSE 
-  reset(p1,p2) {
-    p1.health = 100
-    p2.health = 100
-    this.isOver = false
-    resultDiv.innerText = ''
-    updateGame(p1,p2)
+  reset(p1, p2) {
+    p1.health = 100;
+    p2.health = 100;
+    this.isOver = false;
+    resultDiv.innerText = "";
+    updateGame(p1, p2);
   }
-  
-  // Simulates the whole match until one player runs out of health 
+
   play(p1, p2) {
     this.reset(p1, p2);
-    
-    while (!this.isOver) { // checks whether isOver == false
-      p1.strike(p1,p2, p1.attackDmg)
-      p2.heal(p2)
-      p2.strike(p2,p1, p2.attackDmg);
-      p1.heal(p1)
-    }
-    // Once isOver is TRUE run the declareWinner() method 
-    return this.declareWinner(this.isOver,p1,p2);
-  }
 
+    while (!this.isOver) {
+      p1.strike(p1, p2, p1.attackDmg);
+      p2.heal(p2);
+      p2.strike(p2, p1, p2.attackDmg);
+      p1.heal(p1);
+    }
+
+    return this.declareWinner(this.isOver, p1, p2);
+  }
 }
 
-// Create 2 players using the player class 
-let player1 = new Player('RYU', 100, 15)
-let player2 = new Player('KEN', 100, 15)
+let player1 = new Player("RYU", 100, 15);
+let player2 = new Player("KEN", 100, 15);
 
+let p1 = player1;
+let p2 = player2;
 
-// Save original Player Health in order to reset 
-let p1 = player1
-let p2 = player2
-
-
-
-// Create the game object from the Game class 
 let game = new Game();
-//  Intialize the game by calling updateGame() 
-updateGame(p1,p2)
+updateGame(p1, p2);
 
-// Save original Game Data 
-let gameState = game.isOver
+let gameState = game.isOver;
 
+play.onclick = () => (result.innerText = game.play(p1, p2));
 
-// Add a click listener to the simulate button that runs the play() method on click and pass in the players 
-play.onclick = () => result.innerText = game.play(p1,p2);
+document.addEventListener("keydown", function (e) {
+  if (e.key.toLowerCase() === "q" && p2.health > 0 && game.isOver == false) {
+    p1.strike(p1, p2, p1.attackDmg);
 
-
-// ** Player 1 Controls **
-document.addEventListener('keydown', function(e) {
-  
-  if (e.key.toLowerCase() === "q" && p2.health > 0 && game.isOver == false ){ // it will listen to both "q" & "Q" cause of .toLowerCase()
-    p1.strike(p1, p2, p1.attackDmg)
-    
-    document.getElementById('p1attack').play(); // play attack sound
+    document.getElementById("p1attack").play();
   }
 });
 
-document.addEventListener('keydown', function(e) {
-    
-  if (e.key.toLowerCase() === "a" && p2.health > 0 ){ // it will listen to both "a" & "A" cause of .toLowerCase()
-   p1.heal(p1)
-    
-   document.getElementById('p1heal').play(); // play heal sound
+document.addEventListener("keydown", function (e) {
+  if (e.key.toLowerCase() === "a" && p2.health > 0) {
+    p1.heal(p1);
+
+    document.getElementById("p1heal").play();
   }
 });
 
-// ** Player 2 Controls **
-document.addEventListener('keydown', function(e) {
-  
-  if (e.key.toLowerCase() === "p" && p1.health > 0 && game.isOver == false ){ // it will listen to both "p" & "P" cause of .toLowerCase()
-    p2.strike(p2, p1, p2.attackDmg)
-    
-    document.getElementById('p2attack').play(); // play attack sound
+document.addEventListener("keydown", function (e) {
+  if (e.key.toLowerCase() === "p" && p1.health > 0 && game.isOver == false) {
+    p2.strike(p2, p1, p2.attackDmg);
+
+    document.getElementById("p2attack").play();
   }
 });
 
-document.addEventListener('keydown', function(e) {
- 
-  if (e.key.toLowerCase() === "l" && p2.health > 0 ){ // it will listen to both "l" & "L" cause of .toLowerCase()
-    
-   player2.heal(p2)
-  document.getElementById('p2heal').play(); // play heal sound
+document.addEventListener("keydown", function (e) {
+  if (e.key.toLowerCase() === "l" && p2.health > 0) {
+    player2.heal(p2);
+    document.getElementById("p2heal").play();
   }
 });
--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,90 +1,71 @@
-function getComputerChoice() { // function to get random Computer Choice
-    let rpsChoices = ['Rock', 'Paper', 'Scissors']
-    let computerChoice = rpsChoices[Math.floor(Math.random() * 3)]
-    return computerChoice
-  }
-  
-  function getResult(playerChoice, computerChoice) { // function to pit Player Choice against Computer Choice
-      
-    let score;
-  
-    if (playerChoice === computerChoice) {
-      score = 0
-  
-  
-    } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
-      score = 1
-  
-    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-      score = 1
-  
-    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
-      score = 1
-  
-    } else {
-      score = -1
-    }
-  
-    return score
-  }
-  
-  
-  function showResult(score, playerChoice, computerChoice) { // function to update the DOM with the result
-    
-  
-    let result = document.getElementById('result')
-    switch (score) {
-      case -1:
-        result.innerText = `You Lose!`
-        break;
-      case 0:
-        result.innerText = `It's a Draw!`
-        break;
-      case 1:
-        result.innerText = `You Win!`
-        break;
-    }
-  
-    let playerScore = document.getElementById('player-score')
-    let hands = document.getElementById('hands')
-    playerScore.innerText = `${Number(playerScore.innerText) + score}` // show the score(previous score + current score)
-    hands.innerText = `👱 ${playerChoice} vs 🤖 ${computerChoice}` // emojis result
-  }
-  
-  function onClickRPS(playerChoice) { //main function 
-    const computerChoice = getComputerChoice()
-    const score = getResult(playerChoice.value, computerChoice) //It stores the value of getResult in "score" variable
-    showResult(score, playerChoice.value, computerChoice) // function call - to show the Result on the DOM  
-  }
-  
-  function playGame() { // function to actively listen for a click
+function getComputerChoice() {
+  let rpsChoices = ["Rock", "Paper", "Scissors"];
+  let computerChoice = rpsChoices[Math.floor(Math.random() * 3)];
+  return computerChoice;
+}
 
-    let rpsButtons = document.querySelectorAll('.rpsButton')
-    
-    rpsButtons.forEach(rpsButton => {
-      rpsButton.onclick = () => onClickRPS(rpsButton)
-      /*
-      Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked 
-  
-    -> loop through the buttons using a forEach loop
-    -> Add a 'click' event listener to each button
-    -> Call the onClickRPS function every time someone clicks
-    -> Make sure to pass the currently selected rps button as an argument
-      */
-    })
-  
-    
-    let endGameButton = document.getElementById('endGameButton') 
-    endGameButton.onclick = () => endGame() // adds a click listener and to call the endGame function
+function getResult(playerChoice, computerChoice) {
+  let score;
+
+  if (playerChoice === computerChoice) {
+    score = 0;
+  } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+    score = 1;
+  } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+    score = 1;
+  } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+    score = 1;
+  } else {
+    score = -1;
   }
-  
-  function endGame() { // this function resets all the DOM
-    let playerScore = document.getElementById('player-score')
-    let hands = document.getElementById('hands')
-    let result = document.getElementById('result')
-    playerScore.innerText = ''
-    hands.innerText = ''
-    result.innerText = ''
+
+  return score;
+}
+
+function showResult(score, playerChoice, computerChoice) {
+  let result = document.getElementById("result");
+  switch (score) {
+    case -1:
+      result.innerText = `You Lose!`;
+      break;
+    case 0:
+      result.innerText = `It's a Draw!`;
+      break;
+    case 1:
+      result.innerText = `You Win!`;
+      break;
   }
-  
-  playGame()
+
+  let playerScore = document.getElementById("player-score");
+  let hands = document.getElementById("hands");
+  playerScore.innerText = `${Number(playerScore.innerText) + score}`;
+  hands.innerText = `👱 ${playerChoice} vs 🤖 ${computerChoice}`;
+}
+
+function onClickRPS(playerChoice) {
+  const computerChoice = getComputerChoice();
+  const score = getResult(playerChoice.value, computerChoice);
+  showResult(score, playerChoice.value, computerChoice);
+}
+
+function playGame() {
+  let rpsButtons = document.querySelectorAll(".rpsButton");
+
+  rpsButtons.forEach((rpsButton) => {
+    rpsButton.onclick = () => onClickRPS(rpsButton);
+  });
+
+  let endGameButton = document.getElementById("endGameButton");
+  endGameButton.onclick = () => endGame();
+}
+
+function endGame() {
+  let playerScore = document.getElementById("player-score");
+  let hands = document.getElementById("hands");
+  let result = document.getElementById("result");
+  playerScore.innerText = "";
+  hands.innerText = "";
+  result.innerText = "";
+}
+
+playGame();
